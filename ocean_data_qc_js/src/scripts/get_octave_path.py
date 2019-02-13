04b73cd613e894e79f9bb42eee0cb31adde1a0fb
Anton Velo <avelo@iim.csic.es>
2019-02-13 13:56:07 +0100

    python script to get octave path

---
+++ ocean_data_qc_js/src/scripts/get_octave_path.py
@@ -0,0 +1,25 @@
+import shutil
+import sys
+import fnmatch
+import os
+
+OCTAVE_EXECUTABLE = shutil.which('octave')
+if sys.platform == 'win32':
+    if not shutil.which('octave.exe') and not shutil.which('octave-cli.exe'):
+        base_octave = r'C:\Octave'
+        if os.path.isdir(base_octave):
+            try:
+                vdir = os.listdir(base_octave)[0]
+                if os.path.isfile(os.path.join(base_octave,vdir,'bin','octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave,vdir,'bin','octave-cli.exe')
+            except:
+                pass
+else:
+    if not shutil.which('octave'):
+        if os.path.isfile('/usr/local/bin/octave'):
+            OCTAVE_EXECUTABLE = '/usr/local/bin/octave'
+        else:
+            for dname in os.listdir('/Applications'):
+                if fnmatch.fnmatch(dname, 'Octave-*'):
+                    OCTAVE_EXECUTABLE = os.path.join('/Applications', dname, 'Contents/Resources/usr/bin/octave')
+print(OCTAVE_EXECUTABLE)
