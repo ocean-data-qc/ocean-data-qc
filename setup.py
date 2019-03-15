725d484dce60042514affd0c5fd55c5ac8d8a205
Jesus Cacabelos <jcacabelos@iim.csic.es>
2019-03-15 18:54:37 +0100

    Remove conda-meta folder after installing the package because it contains absolute paths. The shebang mapping did not work properly, so it is still commented

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ import os
 from os.path import dirname, exists, join, realpath, relpath
 from setuptools import setup
 from sys import platform
+import shutil
 
 ROOT = dirname(realpath(__file__))
-
+ENV_PATH = os.path.dirname(shutil.which('python'))
+print('-- ENV PATH: {}'.format(ENV_PATH))
 
 def get_package_data():
     print('-- GET PACKAGE DATA')
@@ -44,6 +46,9 @@ requires = [
     'scipy >=1.1.0',
     'tilecloud'
 ]
+
+# TODO: when python setup.py develop >> jupyter lab should be installed as well to make tests
+
 dependency_links = [
     'https://github.com/ocean-data-qc/tilecloud/tarball/master#egg=tilecloud'
 ]
@@ -73,3 +78,43 @@ setup(
     package_data=get_package_data(),
     zip_safe=False,
 )
+
+# TODO: remove conda-meta in the environment folder (removing absolute paths)
+# TODO: remove shebangs
+
+# NOTE: if for some reason they are in readonly mode
+#       https://stackoverflow.com/questions/2656322/shutil-rmtree-fails-on-windows-with-access-is-denied
+conda_meta = os.path.join(ENV_PATH, 'conda-meta')
+if os.path.isdir(conda_meta):
+    print('>> REMOVING CONDA-META: {}'.format(conda_meta))
+    shutil.rmtree(conda_meta)
+
+# fn = 0
+# for root, subdirs, files in os.walk(ENV_PATH):  # root == ENV_PATH
+#     print('>> ROOT: {}'.format(root))
+#     for filename in files:
+#         # TODO: make some filter to do it faster
+
+#         fn += 1
+#         if fn % 100 == 0:
+#             print('>> FILE NUMBER: {}'.format(fn))
+#         file_path = os.path.join(root, filename)
+#         try:
+#             with open(file_path, 'r') as f:
+#                 lines = f.readlines()
+#             with open(file_path, 'w') as f:
+#                 for line in lines:
+#                     f.write(re.sub(r'^#!.*\n', '\n', line))   # TODO: only first line >> faster
+#             # print('>> REPLACEMENT DONE IN: {}'.format(file_path))
+#         except Exception as e:
+#             pass
+#             # print('>> ERROR READING FILE: {}'.format(file_path))
+
+
+# TODO: this is the correct shebang to use >> #!/usr/bin/env python3
+#       https://stackoverflow.com/a/19305076/4891717
+python_map = [
+    '^#! /usr/bin/env python',
+    '^#!python',
+    '^#!.*python.exe',  # absolute paths
+]
