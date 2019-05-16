17321d44af847e189ffa0e9d802765961717deed
Anton Velo <avelo@iim.csic.es>
2019-05-16 09:17:19 +0200

    Modified octave detection scripts for Windows in case different versions were installed together. The latest version will be the one used.

--- ocean_data_qc/octave/octave.py
+++ ocean_data_qc/octave/octave.py
@@ -12,24 +12,27 @@ from bokeh.util.logconfig import bokeh_logger as lg
 
 
 if sys.platform == 'win32':
-    OCTAVE_EXECUTABLE = shutil.which('octave-cli.exe')
-    if not shutil.which('octave.exe') and not shutil.which('octave-cli.exe'):
-        base_octave = r'C:\Octave'
-        if os.path.isdir(base_octave):
-            try:
-                odir = sorted(os.listdir(base_octave), reverse=True)
-                for vdir in odir:
-                    if os.path.isfile(os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')):
-                        OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')
-                        break
-                    elif os.path.isfile(os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')):
-                        OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')
-                        break
-                    elif os.path.isfile(os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')):
-                        OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')
-                        break
-            except:
-                pass
+    base_octave = r'C:\Octave'
+    if os.path.isdir(base_octave):
+        try:
+            odir = sorted(os.listdir(base_octave), reverse=True)
+            for vdir in odir:
+                if os.path.isfile(os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')
+                    break
+                elif os.path.isfile(os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')
+                    break
+                elif os.path.isfile(os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')
+                    break
+        except:
+            pass
+    if OCTAVE_EXECUTABLE=='':
+        if shutil.which('octave-cli.exe'):
+            OCTAVE_EXECUTABLE = shutil.which('octave-cli.exe')
+        elif shutil.which('octave.exe'):
+            OCTAVE_EXECUTABLE = shutil.which('octave.exe')
 else:
     OCTAVE_EXECUTABLE = shutil.which('octave')
     if not shutil.which('octave'):
--- ocean_data_qc_js/src/scripts/get_octave_path.py
+++ ocean_data_qc_js/src/scripts/get_octave_path.py
@@ -6,24 +6,27 @@ import pathlib
 
 OCTAVE_EXECUTABLE = ''
 if sys.platform == 'win32':
-    OCTAVE_EXECUTABLE = shutil.which('octave-cli.exe')
-    if not shutil.which('octave.exe') and not shutil.which('octave-cli.exe'):
-        base_octave = r'C:\Octave'
-        if os.path.isdir(base_octave):
-            try:
-                odir = sorted(os.listdir(base_octave), reverse=True)
-                for vdir in odir:
-                    if os.path.isfile(os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')):
-                        OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')
-                        break
-                    elif os.path.isfile(os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')):
-                        OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')
-                        break
-                    elif os.path.isfile(os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')):
-                        OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')
-                        break
-            except:
-                pass
+    base_octave = r'C:\Octave'
+    if os.path.isdir(base_octave):
+        try:
+            odir = sorted(os.listdir(base_octave), reverse=True)
+            for vdir in odir:
+                if os.path.isfile(os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw64', 'bin', 'octave-cli.exe')
+                    break
+                elif os.path.isfile(os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'bin', 'octave-cli.exe')
+                    break
+                elif os.path.isfile(os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')):
+                    OCTAVE_EXECUTABLE = os.path.join(base_octave, vdir, 'mingw32', 'bin', 'octave-cli.exe')
+                    break
+        except:
+            pass
+    if OCTAVE_EXECUTABLE=='':
+        if shutil.which('octave-cli.exe'):
+            OCTAVE_EXECUTABLE = shutil.which('octave-cli.exe')
+        elif shutil.which('octave.exe'):
+            OCTAVE_EXECUTABLE = shutil.which('octave.exe')
 else:
     OCTAVE_EXECUTABLE = shutil.which('octave')
     if not shutil.which('octave'):
