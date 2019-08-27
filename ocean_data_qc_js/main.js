12a2ce2dd55d11037b65ded54672b5226f49db93
Jesus Cacabelos <jcacabelos@iim.csic.es>
2019-08-27 13:28:35 +0200

    Updated to electron 6.0.4

--- ocean_data_qc_js/main.js
+++ ocean_data_qc_js/main.js
@@ -49,8 +49,9 @@ var main_window = null;      // global reference of the window object
 
 // ----------------------- APP EVENTS -------------------------- //
 
-// Modify this line in the future if there is a better solution
-// or if the issue is solved https://stackoverflow.com/a/57288472/4891717
+// Modify this line in the future if there is a better solution:
+// GH issue: https://github.com/electron/electron/issues/18214
+// SO solution: https://stackoverflow.com/a/57288472/4891717
 app.commandLine.appendSwitch('disable-site-isolation-trials');
 
 app.on('ready', function() {
